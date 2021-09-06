06623ef27d516d61cf7f9cf1d2165c01de49195b
Ritam  Majumdar <hrickmajumdar@gmail.com>
2021-09-06 11:30:34 +0530

    Update CNN.py

--- Models/CNN.py
+++ Models/CNN.py
@@ -1,1 +1,43 @@
+import numpy as np
+import tensorflow as tf
+from keras.models import Sequential, Model, Input
+from keras.layers import Dense, Flatten, Layer, ConvLSTM2D, Bidirectional
+from keras.layers import Dropout, LayerNormalization, MultiHeadAttention, Add
+from keras.layers import LSTM, RNN, Embedding, TimeDistributed, Activation
+from keras.layers.convolutional import Conv1D, Conv2D
+from keras.layers.convolutional import MaxPooling1D, MaxPooling2D
+from keras import backend
 
+def CNN(shape):
+    model = Sequential()
+    model.add(Conv2D(kernel_size=(3,3), filters=32, padding='same',data_format = 'channels_last', activation='swish',input_shape=(shape[0], shape[1], shape[2])))
+    model.add(Conv2D(kernel_size=(3,3), filters=32, padding='same', activation='swish'))
+    model.add(Dropout(0.15))
+    #model.add(MaxPooling2D(pool_size = (2,2)))
+
+    model.add(Conv2D(kernel_size=(3,3), filters=64, padding='same',  activation='swish'))
+    model.add(Conv2D(kernel_size=(3,3), filters=64, padding='same',  activation='swish'))
+    model.add(Dropout(0.15))
+    model.add(MaxPooling2D(pool_size = (2,2)))
+
+    model.add(Conv2D(kernel_size=(3,3), filters=128, padding='same',  activation='swish'))
+    model.add(Conv2D(kernel_size=(3,3), filters=128, padding='same',  activation='swish'))
+    model.add(Dropout(0.15))
+    #model.add(MaxPooling2D(pool_size = (2,2)))
+
+    model.add(Conv2D(kernel_size=(3,3), filters=256, padding='same',  activation='swish'))
+    model.add(Conv2D(kernel_size=(3,3), filters=256, padding='same',  activation='swish'))
+    model.add(Dropout(0.15))
+    model.add(MaxPooling2D(pool_size = (2,2)))
+    
+    model.add(Flatten())
+    model.add(Dense(units = 1000,  activation='swish'))
+    model.add(Dropout(0.25))
+    model.add(Dense(units = 1000,  activation='swish'))
+    model.add(Dropout(0.25))
+    model.add(Dense(units = 1000,  activation='swish'))
+    model.add(Dropout(0.25))
+    model.add(Dense(1))
+    model.add(Activation('linear'))
+
+    return(model)
