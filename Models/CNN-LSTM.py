42b09b7bab3065fbe1192abafbfcfbc2c894a95f
Ritam  Majumdar <hrickmajumdar@gmail.com>
2021-09-06 11:22:40 +0530

    Update CNN-LSTM.py

--- Models/CNN-LSTM.py
+++ Models/CNN-LSTM.py
@@ -8,32 +8,6 @@ from keras.layers.convolutional import Conv1D, Conv2D
 from keras.layers.convolutional import MaxPooling1D, MaxPooling2D
 from keras import backend
 
-class Attention(Layer):
-    
-    def __init__(self, return_sequences=True):
-        self.return_sequences = return_sequences
-        super(Attention,self).__init__()
-        
-    def build(self, input_shape):        
-        self.W=self.add_weight(name="att_weight", shape=(input_shape[-1],1),initializer="normal")
-        self.b=self.add_weight(name="att_bias", shape=(input_shape[1],1),initializer="zeros")        
-        super(Attention,self).build(input_shape)
-        
-    def call(self, x):        
-        e = backend.tanh(backend.dot(x,self.W)+self.b)
-        a = backend.softmax(e, axis=1)
-        output = x*a
-        
-        if self.return_sequences:
-            return output
-          
-        return backend.sum(output, axis=1)
-
-    def get_config(self):
-        config = super().get_config().copy()
-        config.update({'return_sequences': self.return_sequences})
-        return config
-
- def CNN_LSTM(shape):
+def CNN_LSTM(shape):
     model = Sequential()
     return(model)
